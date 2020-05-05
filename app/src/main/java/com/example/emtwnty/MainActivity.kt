ede7149e55fe134858f76bbd01ceb3b6c6524ef4
emtwnty <muizahsanu@gmail.com>
2020-05-05 16:21:05 +0700

    First-commit: Create Project

---
+++ app/src/main/java/com/example/emtwnty/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.emtwnty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/* 5 Mei 2020
+* Nama: Muiz Ahsanu Haqi
+* Kelas: IF-5
+* Nim: 10117199 */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
